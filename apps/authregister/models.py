a5a8acbe1b58941ce844c12e69ba4c54a3481f5b
aramis <aramismario@gmail.com>
2019-12-18 14:00:16 -0500

    using abstract models to remove repeated code

--- apps/authregister/models.py
+++ apps/authregister/models.py
@@ -11,13 +11,22 @@ from datetime import timedelta
 import jwt, json
 import os
 
-class Admins(models.Model):
+
+class Employee(models.Model):
+    identificationnumber = models.CharField(db_column='identificationNumber', max_length=30)  # Field name made lowercase.
+    firstname = models.CharField(db_column='firstName', max_length=60)  # Field name made lowercase.
+    lastname = models.CharField(db_column='lastName', max_length=60)  # Field name made lowercase.
     email = models.CharField(max_length=60)
     password = models.CharField(max_length=64)
+    socialsecuritynumber = models.CharField(db_column='socialSecurityNumber', max_length=11)  # Field name made lowercase.
 
     class Meta:
-        managed = False
-        db_table = 'admins'
+        abstract = True
+
+class Auth(models.Model):
+
+    class Meta:
+        abstract = True
 
     is_authenticated = False
 
@@ -35,7 +44,16 @@ class Admins(models.Model):
         return self.generate_jwt_token()
 
 
-class Customers(models.Model):
+class Admins(Auth):
+    email = models.CharField(max_length=60)
+    password = models.CharField(max_length=64)
+
+    class Meta:
+        managed = False
+        db_table = 'admins'
+
+
+class Customers(Auth):
     identificationnumber = models.CharField(db_column='identificationNumber', max_length=30)  # Field name made lowercase.
     firstname = models.CharField(db_column='firstName', max_length=60)  # Field name made lowercase.
     lastname = models.CharField(db_column='lastName', max_length=60)  # Field name made lowercase.
@@ -46,71 +64,17 @@ class Customers(models.Model):
         managed = False
         db_table = 'customers'
 
-    is_authenticated = False
 
-    def generate_jwt_token(self):
-        dt = datetime.now() + timedelta(minutes = 3)
-        token = jwt.encode({
-            'id':self.pk,
-            'email':self.email,
-            'exp':dt
-        },os.environ['SECRETKEY'])
-        return token
 
-    @property
-    def token(self):
-        return self.generate_jwt_token()
-
-class Mechanics(models.Model):
-    identificationnumber = models.CharField(db_column='identificationNumber', max_length=30)  # Field name made lowercase.
-    firstname = models.CharField(db_column='firstName', max_length=60)  # Field name made lowercase.
-    lastname = models.CharField(db_column='lastName', max_length=60)  # Field name made lowercase.
-    email = models.CharField(max_length=60)
-    password = models.CharField(max_length=64)
-    socialsecuritynumber = models.CharField(db_column='socialSecurityNumber', max_length=11)  # Field name made lowercase.
+class Mechanics(Employee, Auth):
 
     class Meta:
         managed = False
         db_table = 'mechanics'
 
-    is_authenticated = False
 
-    def generate_jwt_token(self):
-        dt = datetime.now() + timedelta(minutes = 3)
-        token = jwt.encode({
-            'id':self.pk,
-            'email':self.email,
-            'exp':dt
-        },os.environ['SECRETKEY'])
-        return token
-
-    @property
-    def token(self):
-        return self.generate_jwt_token()
-
-class Receptionist(models.Model):
-    identificationnumber = models.CharField(db_column='identificationNumber', max_length=30)  # Field name made lowercase.
-    firstname = models.CharField(db_column='firstName', max_length=60)  # Field name made lowercase.
-    lastname = models.CharField(max_length=60)
-    email = models.CharField(max_length=60)
-    password = models.CharField(max_length=64)
-    socialsecuritynumber = models.CharField(db_column='socialSecurityNumber', max_length=11)  # Field name made lowercase.
+class Receptionist(Employee, Auth):
 
     class Meta:
         managed = False
         db_table = 'receptionist'
-
-    is_authenticated = False
-
-    def generate_jwt_token(self):
-        dt = datetime.now() + timedelta(minutes = 3)
-        token = jwt.encode({
-            'id':self.pk,
-            'email':self.email,
-            'exp':dt
-        },os.environ['SECRETKEY'])
-        return token
-
-    @property
-    def token(self):
-        return self.generate_jwt_token()
